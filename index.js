49b52f93b93e982ba1249fbf8e57eb765868704d
Travis <tjwokasch@gmail.com>
2021-07-06 13:27:45 -0600

    commit tree 758e5f08c294af0ebc6403fe162c3e3c37bb79c6

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+  return "This one is on me!"
+  }
+  else if (400 < ride && ride < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (ride > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'?'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case tip = 'generous' :
+      return 'Thank you so much.'
+     case tip = 'not as generous' :
+       return 'Thank you.'
+     default:
+       return "Bye."
+  }
 }
